3f91ce6bb3744ad6b2cac6e9d27a69d0e13f5894
Leonid Khlebushchev <khlebushchev.ls@infotek.ru>
2022-01-31 21:13:52 +0300

    Task:32373027 renamed test files. Version command line option checking. test_ubus_api updated (config modification and ubus answer checking)

---
+++ test/test.py
@@ -0,0 +1,156 @@
+import ubus
+import os
+import time
+
+
+
+
+def test_conf_existance():
+    ret = False
+
+    try:
+        if not os.path.isfile("/etc/config/owrt_sensor_value"):
+            raise ValueError('Config not found')
+
+        ret = True
+
+    except Exception as ex:
+        print(ex)
+
+    assert ret
+
+def test_conf_valid():
+    ret = False
+
+    try:
+        ubus.connect()
+
+        confvalues = ubus.call("uci", "get", {"config": "owrt_sensor_value"})
+        for confdict in list(confvalues[0]['values'].values()):
+            #check globals
+            if confdict['.name'] == 'globals':
+                assert confdict['default_memo'] == 'Sensor'
+                assert confdict['status'] == ['0.Норма', '1.Таймаут', '2.Ошибка']
+                assert confdict['default_timeout'] == '5'
+                assert confdict['default_period'] == '1'
+
+            #check sensor_prototype_snmp
+            if confdict['.name'] == 'sensor_prototype_snmp':
+                assert confdict['memo'] == 'Sensor'
+                assert confdict['precision'] == '0'
+                assert confdict['proto'] == 'SNMP'
+                assert confdict['community'] == '0'
+                assert confdict['address'] == '0'
+                assert confdict['port'] == '0'
+                assert confdict['oid'] == '0'
+                assert confdict['type_oid'] == '0'
+                assert confdict['timeout'] == '5'
+                assert confdict['period'] == '1'
+
+        ubus.disconnect()
+
+        ret = True
+
+    except Exception as ex:
+        print(ex)
+
+    assert ret
+
+def test_running():
+    ret = False
+
+    try:
+        ubus.connect()
+
+        for l in list(ubus.objects().keys()):
+            if l == 'owrt_sensor_value':
+                ret = True
+                break
+
+        ubus.disconnect()
+    except Exception as ex:
+        print(ex)
+
+    assert ret
+
+def test_ubus_api():
+    ret = False
+
+    try:
+        #set config items
+        if os.system("uci set owrt_sensor_value.td28933=info"):
+            raise ValueError("Can't create new section")
+
+        if os.system("uci set owrt_sensor_value.td28933.memo='Out_temperature'"):
+            raise ValueError("Can't set option memo")
+
+        if os.system("uci set owrt_sensor_value.td28933.unit='C'"):
+            raise ValueError("Can't set option unit")
+
+        if os.system("uci set owrt_sensor_value.td28933.precision='2'"):
+            raise ValueError("Can't set option precision")
+
+        if os.system("uci set owrt_sensor_value.td28933.proto='SNMP'"):
+            raise ValueError("Can't set option proto")
+
+        if os.system("uci set owrt_sensor_value.td28933.community='ping32'"):
+            raise ValueError("Can't set option community")
+
+        if os.system("uci set owrt_sensor_value.td28933.address='125.227.188.172'"):
+            raise ValueError("Can't set option address")
+
+        if os.system("uci set owrt_sensor_value.td28933.oid='.1.3.6.1.4.1.25728.8800.1.1.2.1'"):
+            raise ValueError("Can't set option oid")
+
+        if os.system("uci set owrt_sensor_value.td28933.type_oid='Integer'"):
+            raise ValueError("Can't set option type_oid")
+
+        if os.system("uci set owrt_sensor_value.td28933.port='31132'"):
+            raise ValueError("Can't set option port")
+
+        if os.system("uci set owrt_sensor_value.td28933.timeout='3'"):
+            raise ValueError("Can't set option timeout")
+
+        if os.system("uci set owrt_sensor_value.td28933.period='1'"):
+            raise ValueError("Can't set option period")
+
+        if os.system("uci set owrt_sensor_value.td28933.period='1'"):
+            raise ValueError("Can't set option period")
+
+        if os.system("uci commit owrt_sensor_value"):
+            raise ValueError("Can't commit config owrt_sensor_value")
+
+        #send commit signal for module
+        if os.system("ubus send commit '{\"config\":\"owrt_sensor_value\"}'"):
+            raise ValueError("Can't send commit signal to owrt_sensor_value")
+
+        #wait for sensor getting value
+        time.sleep(5)
+
+        ubus.connect()
+
+        result = ubus.call('owrt_sensor_value', 'get_value', {"id_sensor":"td28933"})[0]
+
+        ubus.disconnect()
+
+        #check result
+        expected_keys = [ 'value', 'unit', 'status' ]
+        keys_length = len(list(result.keys()))
+
+        if keys_length != 3:
+            raise ValueError("Bad key length!")
+
+        for l in list(result.keys()):
+            if not l in expected_keys:
+                raise ValueError("Bad key \"" + l + "\"")
+
+        ret = True
+    except Exception as ex:
+        print(ex)
+
+    #delete section from config
+    os.system("uci delete owrt_sensor_value.td28933")
+    os.system("uci commit owrt_sensor_value")
+    os.system("ubus send commit '{\"config\":\"owrt_sensor_value\"}'")
+
+    assert ret
--- test/test_api.py
+++ test/test_api.py
@@ -1,146 +1,23 @@
-import ubus
+#!/usr/bin/python3
 import os
+import sys
 
 
 
 
-def get_package_info():
-    ret = dict()
+def help():
+    print("Usage: " + sys.argv[0] + " <version>")
 
-    with open('../Makefile.inf') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
+if __name__ == "__main__":
+    expected_version = "0.1"
 
-            if not line:
-                continue
+    if len(sys.argv) != 2:
+        help()
+        sys.exit(-1)
 
-            if line.startswith('PKG_NAME='):
-                line = line.replace('PKG_NAME=', '')
+    if sys.argv[1] != expected_version:
+        print("Wrong version! Expected \"" + expected_version + "\"")
+        sys.exit(-2)
 
-                if line[0] == '"' or line[0] == "'":
-                    line = line[1:]
-
-                if line[len(line) - 1] == '"' or line[len(line) - 1] == "'":
-                    line = line[:-1]
-
-                ret['name'] = line
-                continue
-
-            if line.startswith('PKG_VERSION='):
-                line = line.replace('PKG_VERSION=', '')
-
-                if line[0] == '"' or line[0] == "'":
-                    line = line[1:]
-
-                if line[len(line) - 1] == '"' or line[len(line) - 1] == "'":
-                    line = line[:-1]
-
-                ret['version'] = line
-                continue
-
-            if line.startswith('PKG_RELEASE='):
-                line = line.replace('PKG_RELEASE=', '')
-
-                if line[0] == '"' or line[0] == "'":
-                    line = line[1:]
-
-                if line[len(line) - 1] == '"' or line[len(line) - 1] == "'":
-                    line = line[:-1]
-
-                ret['release'] = line
-                continue
-
-    return ret
-
-def print_header():
-    info = get_package_info()
-    print('\n')
-    print('Testing ' + info['name'] + " version " + info['version'] + " release " + info['release'] + "\n...")
-    print('\n')
-
-print_header()
-
-def test_conf_existance():
-    ret = False
-
-    try:
-        if not os.path.isfile("/etc/config/owrt_sensor_value"):
-            raise ValueError('Config not found')
-
-        ret = True
-
-    except Exception as ex:
-        print(ex)
-
-    assert ret
-
-def test_conf_valid():
-    ret = False
-
-    try:
-        ubus.connect()
-
-        confvalues = ubus.call("uci", "get", {"config": "owrt_sensor_value"})
-        for confdict in list(confvalues[0]['values'].values()):
-            #check globals
-            if confdict['.name'] == 'globals':
-                assert confdict['default_memo'] == 'Sensor'
-                assert confdict['status'] == ['0.Норма', '1.Таймаут', '2.Ошибка']
-                assert confdict['default_timeout'] == '5'
-                assert confdict['default_period'] == '1'
-
-            #check sensor_prototype_snmp
-            if confdict['.name'] == 'sensor_prototype_snmp':
-                assert confdict['memo'] == 'Sensor'
-                assert confdict['precision'] == '0'
-                assert confdict['proto'] == 'SNMP'
-                assert confdict['community'] == '0'
-                assert confdict['address'] == '0'
-                assert confdict['port'] == '0'
-                assert confdict['oid'] == '0'
-                assert confdict['type_oid'] == '0'
-                assert confdict['timeout'] == '5'
-                assert confdict['period'] == '1'
-
-        ubus.disconnect()
-
-        ret = True
-
-    except Exception as ex:
-        print(ex)
-
-    assert ret
-
-def test_running():
-    ret = False
-
-    try:
-        ubus.connect()
-
-        for l in list(ubus.objects().keys()):
-            if l == 'owrt_sensor_value':
-                ret = True
-                break
-
-        ubus.disconnect()
-    except Exception as ex:
-        print(ex)
-
-    assert ret
-
-def test_ubus_api():
-    ret = False
-
-    try:
-        ubus.connect()
-
-        ubus.call('owrt_sensor_value', 'get_value', {"id_sensor":"td28933"})
-
-        ubus.disconnect()
-
-        ret = True
-    except Exception as ex:
-        print(ex)
-
-    assert ret
+    os.system("pytest --capture=no -v test.py")
+    sys.exit(0)
